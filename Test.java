3b8498576ede8c5c33f8d076a21ddcaf81f2b28a
Shivakant Singh <92222718+Shivakant6387@users.noreply.github.com>
2022-11-12 12:04:41 +0530

    Add files via upload

---
+++ Test.java
@@ -0,0 +1,10 @@
+package com.ConditionOP.com;
+
+public class Test {
+    public static void main(String[] args) {
+        int a=10,b=15,c=20;
+        System.out.println(a<b && a<c);
+        System.out.println(a<b && a>c);
+        System.out.println(a<b || a>c);
+    }
+}
